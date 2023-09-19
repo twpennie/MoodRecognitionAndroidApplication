25e9e31528772cea499dcfb6467ea5342e861992
Thomas <twpennie@gmail.com>
2023-09-18 23:23:34 -0400

    Face detected and highlighted via a rectangle. TODO: image is stretched when rectangle is drawn onto the screen

--- app/src/main/java/com/example/moodrecognition/AnalyzeFaceActivity.java
+++ app/src/main/java/com/example/moodrecognition/AnalyzeFaceActivity.java
@@ -1,16 +1,22 @@
 package com.example.moodrecognition;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,7 +28,11 @@ import com.google.mlkit.vision.face.FaceDetector;
 import com.google.mlkit.vision.face.FaceDetectorOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class AnalyzeFaceActivity extends AppCompatActivity {
 
@@ -30,7 +40,8 @@ public class AnalyzeFaceActivity extends AppCompatActivity {
     String stringUri;
     Uri imageUri;
     InputImage image;
-
+    Bitmap imageBitmap;
+    ArrayList<Rect> faceRectangles = new ArrayList<Rect>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,12 +80,20 @@ public class AnalyzeFaceActivity extends AppCompatActivity {
                 .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
                 .build();
 
-        // convert URI to InputImage to be passed to firebase model
+        // convert the image to a bitmap to use dimensions for drawing face rectangle
+        imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+
         try {
             image = InputImage.fromFilePath(this.getApplicationContext(), imageUri);
+
         } catch (IOException e){
             e.printStackTrace();
         }
+
+
+
+
+
 
         // get instance of FaceDetector
         FaceDetector detector = FaceDetection.getClient(highAccuracyOpts);
@@ -87,7 +106,13 @@ public class AnalyzeFaceActivity extends AppCompatActivity {
                                     @Override
                                     public void onSuccess(List<Face> faces) {
                                         // successfully detected face
-                                        getFaceInfo(faces);
+                                        for (Face face : faces){
+                                            Rect bounds = face.getBoundingBox();
+                                            faceRectangles.add(bounds);
+                                            Log.d("getFaceInfo", "Face detected");
+                                        }
+                                        drawRects();
+
                                     }
                                 })
                         .addOnFailureListener(
@@ -98,18 +123,31 @@ public class AnalyzeFaceActivity extends AppCompatActivity {
                                         e.printStackTrace();
                                     }
                                 });
-
-
-
-
     }
 
-    private void getFaceInfo(List<Face> faces) {
+    private void drawRects() {
 
-        for (Face face : faces){
-            Rect bounds = face.getBoundingBox();
-            Log.d("getFaceInfo", "Face detected");
+        // create a Canvas for drawing rectangle around detected faces
+        // background bitmap must be mutable for use in canvas
+        Bitmap mutableBm = imageBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(mutableBm);
+
+        for (Rect bounds : faceRectangles){
+            // draw rectangle around face to show detection
+            Paint myPaint = new Paint();
+            myPaint.setColor(Color.rgb(0, 121, 107));
+            myPaint.setStrokeWidth(15);
+            myPaint.setStyle(Paint.Style.STROKE);
+
+            canvas.drawRect(bounds, myPaint);
+
+            LinearLayout rectangleLayout = (LinearLayout) findViewById(R.id.rectangleView);
+            rectangleLayout.setBackground(new BitmapDrawable(mutableBm));
         }
 
+
+
+
     }
+
 }
